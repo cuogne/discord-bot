fdd0aa81acbdda7db6fc022b427e296113d42307
cuogne <cuog.nc116@gmail.com>
2026-03-22 00:09:07 +0700

    fix(giaxang): fix getToday to use VN timezone

--- commands/giaxang/utils/getToday.js
+++ commands/giaxang/utils/getToday.js
@@ -1,8 +1,12 @@
 export function getToday() {
   const today = new Date();
-  const day = String(today.getDate()).padStart(2, '0');
-  const month = String(today.getMonth() + 1).padStart(2, '0');
-  const year = today.getFullYear();
+  const todayVNTime = new Date(today.toLocaleString('en-US', { 
+    timeZone: 'Asia/Ho_Chi_Minh' 
+  }));
+  
+  const day = String(todayVNTime.getDate()).padStart(2, '0');
+  const month = String(todayVNTime.getMonth() + 1).padStart(2, '0');
+  const year = todayVNTime.getFullYear();
 
   return `${year}-${month}-${day}`; // 2026-03-21
 }
